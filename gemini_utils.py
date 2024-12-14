0305de478056ac16e2eae459df29bd8bd8d74af1
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-12-14 19:57:17 +0900

    add auto generate title when not inputing description

--- gemini_utils.py
+++ gemini_utils.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API"))
 model = genai.GenerativeModel("gemini-1.5-flash")
-def generate_press_description(prompt):
+def generate_press_input(prompt):
   response = model.generate_content(prompt)
   return response.text
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.responses import JSONResponse
 from mongodb_utils import fetch_all_presses, fetch_press_by_id, insert_press
 
 from models import PressRelease
-from gemini_utils import generate_press_description
+from gemini_utils import generate_press_input
 from x_api_utils import get_x_post
 
 app = FastAPI()
@@ -33,7 +33,7 @@ async def get_press_by_id(id: str):
 
 @app.post("/press_releases")
 async def create_press(resource: PressReleaseCreate):
-  prompt = "日本語でこの投稿に基づいてプレスリリースを作成してください。"
+  description_prompt = "日本語でこの投稿に基づいてプレスリリースを作成してください。"
 
   if resource.sns_url and not resource.sns_url.startswith("https://x.com/"):
     raise HTTPException(status_code=400, detail="sns_url must be from X(formerly Twitter)")
@@ -44,7 +44,7 @@ async def create_press(resource: PressReleaseCreate):
     if x_data:
       try:
         x_text = x_data['data']['text']
-        description_input = generate_press_description(prompt + x_text)
+        description_input = generate_press_input(description_prompt + x_text)
       except KeyError:
           print("Error: Could not find 'text' in the response. The API response structure might have changed.")
           print(json.dumps(tweet_data, indent=4)) #Show the response for debugging
@@ -55,8 +55,13 @@ async def create_press(resource: PressReleaseCreate):
   else:
     description_input = resource.description
 
+  if not resource.title:
+    title_input = generate_press_input("日本語で一文で以下の記事にタイトルをお付けください" + description_input)
+  else:
+    title_input = resource.title
+
   press = {
-    "title": resource.title,
+    "title": title_input,
     "description": description_input,
     "uid": resource.uid,
     "sns_url": resource.sns_url,
