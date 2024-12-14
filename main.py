ddd83e9881778a01d50202996a9508b884a5cb91
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-12-14 16:04:38 +0900

    create press api feature

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-from mongodb_utils import fetch_all_presses, fetch_press_by_id
+from pydantic import BaseModel
+from mongodb_utils import fetch_all_presses, fetch_press_by_id, insert_press
 
 from models import PressRelease
 
 app = FastAPI()
+
+class PressReleaseCreate(BaseModel):
+  title: str
+  description: str
+  uid: str
+  image: list[str]
 
 @app.get("/")
 async def root():
@@ -18,3 +25,15 @@ async def get_all_presses():
 async def get_press_by_id(id: str):
     press = fetch_press_by_id(id)
     return press
+
+@app.post("/press_releases")
+async def create_press(resource: PressReleaseCreate):
+  press = {
+    "title": resource.title,
+    "content": resource.content,
+    "uid": resource.uid,
+    "image": resource.image
+  }
+  
+  inserted_id = insert_press(press)
+  return JSONResponse(content={"inserted_id": inserted_id}, status_code=status.HTTP_201_CREATED)
